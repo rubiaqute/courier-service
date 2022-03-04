0e2826fc1e2fa7c8eb38c5f8a44cc3efb298646d
rubiaqute <rubiaqute@gmail.com>
2022-03-05 00:14:46 +0300

    feat: start adding columns

--- couriers/src/app/app.component.ts
+++ couriers/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ColDef, GridApi, GridReadyEvent } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'couriers';
+  private gridApi!: GridApi;
+  columnDefs: ColDef[] = [
+    { headerName: '№ заказа' , field: 'id'},
+    { headerName: 'Дата' , field:'date'},
+    { headerName: 'Отправитель', field:'sender', sortable:true, filter:true},
+    { headerName: 'Получатель', field:'receiver', sortable:true, filter:true},
+    { headerName: 'Статус', field: 'status', sortable:true, filter:true},
+
+];
+
+onGridReady(params: GridReadyEvent) {
+  this.gridApi = params.api;
+  this.gridApi.sizeColumnsToFit()
+}
+rowData = [
+  {id: 1, date: '02.02.2022', sender: "ООО Рога и Копыта", receiver:"ООО Хвосты и Уши", status:"Выполняется"}
+    // { make: 'Toyota', model: 'Celica', price: 35000 },
+    // { make: 'Ford', model: 'Mondeo', price: 32000 },
+    // { make: 'Porsche', model: 'Boxter', price: 72000 }
+];
 }
