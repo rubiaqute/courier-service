e70140cc6996dbde61e0ecdde69803268b51df57
rubiaqute <rubiaqute@gmail.com>
2022-03-06 01:41:16 +0300

    feat: add validation

--- couriers/src/app/components/require-base/form/form.component.ts
+++ couriers/src/app/components/require-base/form/form.component.ts
@@ -4,24 +4,60 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  form:FormGroup
-  constructor(){
+  form: FormGroup;
+  constructor() {
     this.form = new FormGroup({});
   }
   ngOnInit(): void {
     this.form = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(6)]),
-      phone: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.email, Validators.required, Validators.minLength(6)])
+      name: new FormControl('', [Validators.required, Validators.minLength(6), this.validateName]),
+      phone: new FormControl('', [Validators.required, this.validatePhone]),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      weight: new FormControl('1'),
+      receiver: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        this.validateName
+      ]),
+      receiverAddresse: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+      receiverPhone: new FormControl('', [Validators.required, this.validatePhone]),
+      sender: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        this.validateName
+      ]),
+      senderAddresse: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+      senderPhone: new FormControl('', [Validators.required, this.validatePhone]),
+      payment: new FormControl('client'),
     });
   }
-  submit(){
-console.log("form submitted: ", this.form)
-const newRequire={...this.form.value}
-console.log(newRequire)
+  submit() {
+    const newRequire = { ...this.form.value };
+    console.log(newRequire);
   }
-
+  validateName(control:FormControl){
+    if (!/^[a-zA-Zа-яА-Я'" ]+[a-zA-Zа-яА-Я]?$/.test(control.value)&&control.value!==''){
+      return {
+        wrongName:true
+      }
+    }
+    return null;
+  }
+  validatePhone(control:FormControl){
+    if (!/^\+?[0-9 ()-]{6,14}?$/.test(control.value)&&control.value!==''){
+      return {
+        wrongPhone:true
+      }
+    }
+    return null;
+  }
 }
